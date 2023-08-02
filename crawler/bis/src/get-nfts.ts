1d6dfbcb00428d19b07d88b60b15d7af64d1d638
BillyJean04 <bozhozda@gmail.com>
2023-08-02 18:05:40 +0300

    refactor: move url to URL object

--- crawler/bis/src/get-nfts.ts
+++ crawler/bis/src/get-nfts.ts
@@ -2,11 +2,10 @@ import * as Prv from './private-types'
 import { transformData } from './transform'
 
 export async function getNFTs(wallet: string) {
-  const baseUrl = 'https://ordapi.bestinslot.xyz/v1'
+  const baseUrl = 'https://ordapi.bestinslot.xyz/v1/'
+  const url = new URL(`get_inscriptions/${wallet}`, baseUrl)
 
-  const data = await fetch(`${baseUrl}/get_inscriptions/${wallet}`).then(
-    (res) => res.json() satisfies Promise<Prv.NftDataApi[]>,
-  )
+  const data = await fetch(url).then((res) => res.json() satisfies Promise<Prv.NftDataApi[]>)
   const filtered = data.filter((elem) => elem.content_type.startsWith('image'))
 
   return filtered.map((nft) => transformData(nft))
