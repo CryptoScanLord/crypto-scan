11de9882fd42fbc66137ff03deb66633bc3a31ab
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-08-11 21:19:08 +0300

    fix: conflicts and colors fix

--- client/shared/theme/schema/src/input.ts
+++ client/shared/theme/schema/src/input.ts
@@ -2,21 +2,23 @@ import type { ThemeOptions } from '@mui/material'
 
 export const MuiInput = {
   styleOverrides: {
-    root: {
-      padding: '4px 10px 4px 10px',
-      color: '#8F9BB7',
-      fontWeight: 'lighter',
-      fontSize: 'small',
-      borderRadius: '8px',
-      backgroundColor: '#0E1330',
-      border: 'solid 1px #222745',
-      ':before': {
-        content: 'none',
-      },
-      ':after': {
-        transition: 'none',
-        border: 'none',
-      },
+    root: ({ theme }) => {
+      return {
+        padding: '4px 10px 4px 10px',
+        color: `${theme.palette.text}`,
+        fontWeight: 'lighter',
+        fontSize: 'small',
+        borderRadius: '8px',
+        backgroundColor: `${theme.palette.background.paper}`,
+        border: `solid 1px ${theme.palette.divider}`,
+        ':before': {
+          content: 'none',
+        },
+        ':after': {
+          transition: 'none',
+          border: 'none',
+        },
+      }
     },
   },
 } satisfies NonNullable<ThemeOptions['components']>['MuiInput']
