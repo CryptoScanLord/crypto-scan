4b18e18192ea74e90b6e2dbe061c0dfe0ae034eb
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-08-11 13:08:42 +0300

    feat/add-mui-input

--- client/shared/theme/schema/src/input.ts
+++ client/shared/theme/schema/src/input.ts
@@ -1,5 +1,22 @@
 import type { ThemeOptions } from '@mui/material'
 
 export const MuiInput = {
-  styleOverrides: {},
-} satisfies ThemeOptions['components']['MuiButton']
+  styleOverrides: {
+    root: {
+      padding: '4px 10px 4px 10px',
+      color: '#8F9BB7',
+      fontWeight: 'lighter',
+      fontSize: 'small',
+      borderRadius: '8px',
+      backgroundColor: '#0E1330',
+      border: 'solid 1px #222745',
+      ':before': {
+        content: 'none',
+      },
+      ':after': {
+        transition: 'none',
+        border: 'none',
+      },
+    },
+  },
+} satisfies NonNullable<ThemeOptions['components']>['MuiInput']
