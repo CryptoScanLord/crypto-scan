f34af2b2dd3ef42e821ac0610afd4297416bc87e
Danil Karpenko <limpix31@gmail.com>
2023-08-14 10:40:56 +0200

    feat: cache module

--- api/entrypoint/src/app.module.ts
+++ api/entrypoint/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { HelloModule } from '@api/hello-module'
 import { AuthModule } from '@lib/auth-nestjs'
+import { CacheModule } from '@nestjs/cache-manager'
 
 @Module({
-  imports: [AuthModule, HelloModule],
+  imports: [CacheModule.register(), AuthModule, HelloModule],
 })
 export class AppModule {}
