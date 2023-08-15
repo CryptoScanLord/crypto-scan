4e1eddcc89996ba5ce0a40316190bd0083145760
Danil Karpenko <limpix31@gmail.com>
2023-08-15 15:08:38 +0200

    feat: graph api

--- api/wallet/src/wallet.controller.ts
+++ api/wallet/src/wallet.controller.ts
@@ -1,6 +1,64 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, InternalServerErrorException, Param } from '@nestjs/common'
+import { getHistory } from '@crawler/blockchain'
+import { getPriceHistory } from '@crawler/coindesk'
+
+const DAY = 24 * 60 * 60 * 1000
 
 @Controller('wallet/:wallet')
 export class WalletController {
+  @Get('graph')
+  async getGraph(@Param('wallet') wallet: string) {
+    const actualPrice = await fetch('https://ordapi.bestinslot.xyz/v1/btc_price').then((res) => res.text().then(Number))
 
+    const { entries, balance } = await getHistory(wallet)
+
+    if (entries.length === 0) {
+      return []
+    }
+
+    const first = entries.at(0)!
+    const last = entries.at(-1) ?? first
+
+    const [endRange] = first
+    const [startRange] = last
+
+    const prices = await getPriceHistory(new Date(startRange - DAY), new Date(endRange + DAY))
+
+    prices.reverse()
+
+    if (prices.length === 0) {
+      throw new InternalServerErrorException(null, 'Prices array is empty')
+    }
+
+    let iterableBalance = balance
+
+    let priceIdx = 0
+    let lastPrice = prices[0]!
+
+    const mappedHistory = entries
+      .map(([entryTime, delta]) => {
+        if (!lastPrice) {
+          return undefined
+        }
+
+        while (lastPrice[0] > entryTime) {
+          lastPrice = prices[++priceIdx]!
+
+          if (!lastPrice) {
+            return undefined
+          }
+        }
+
+        const [, price] = lastPrice
+
+        iterableBalance -= delta
+
+        return [entryTime, (price * iterableBalance) / 100000000]
+      })
+      .filter(Boolean)
+
+    mappedHistory.unshift([Date.now(), (actualPrice * balance) / 100000000])
+
+    return mappedHistory
+  }
 }
