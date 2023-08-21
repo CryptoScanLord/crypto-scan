7e78d55e4d49cc33e5b61fd5a277da0c703d6394
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-08-21 10:49:55 +0700

    feat: add pagination component

--- client/shared/ui/pagination/src/index.tsx
+++ client/shared/ui/pagination/src/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, MouseEvent, useState } from 'react'
-import { ToggleButton, ToggleButtonGroup } from '@mui/material'
+import { Box, ButtonBase } from '@mui/material'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Routes } from './privateTypes'
 
@@ -7,30 +7,14 @@ const Pagination: FC = () => {
   const { pathname } = useLocation()
   const navigate = useNavigate()
 
-  const [value, setValue] = useState<Routes | null>(pathname as Routes)
-
-  const handleChange = (event: MouseEvent<HTMLElement>, newPage: Routes | null) => {
-    if (newPage !== null) {
-      setValue(newPage)
-      navigate(newPage)
-    }
-  }
 
   return (
-    <ToggleButtonGroup value={value} exclusive onChange={handleChange} aria-label='change page'>
-      <ToggleButton value='/portfolio' aria-label='portfolio page'>
-        Portfolio
-      </ToggleButton>
-      <ToggleButton value='/nfts' aria-label='nfts page'>
-        NFTs
-      </ToggleButton>
-      <ToggleButton value='/brc-20' aria-label='brc-20 page'>
-        BRC-20
-      </ToggleButton>
-      <ToggleButton value='/transactions' aria-label='transactions page'>
-        Transactions
-      </ToggleButton>
-    </ToggleButtonGroup>
+    <Box>
+      <ButtonBase>Portfolio</ButtonBase>
+      <ButtonBase>NFTs</ButtonBase>
+      <ButtonBase>BRC-20</ButtonBase>
+      <ButtonBase>Transactions</ButtonBase>
+    </Box>
   )
 }
 
