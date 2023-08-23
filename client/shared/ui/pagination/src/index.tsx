8dba7f2a6d0731b2a8da60ba6caa91aab3907320
Saveliy Maslakov <93765422+maslakovSaveliy@users.noreply.github.com>
2023-08-23 18:47:50 +0300

    Update client/shared/ui/pagination/src/index.tsx
    Co-authored-by: Danil Karpenko <81296950+LIMPIX31@users.noreply.github.com>

--- client/shared/ui/pagination/src/index.tsx
+++ client/shared/ui/pagination/src/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Box } from '@mui/material'
+import Box from '@mui/material/Box'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Routes } from './privateTypes'
 import { PaginationButton } from './paginationButton'
