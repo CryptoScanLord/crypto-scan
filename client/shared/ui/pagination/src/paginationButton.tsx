feb8451a6b227b8868e77f1154b1aad931cc86f3
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-08-24 19:40:21 +0300

    fix: props fix

--- client/shared/ui/pagination/src/paginationButton.tsx
+++ client/shared/ui/pagination/src/paginationButton.tsx
@@ -5,24 +5,24 @@ interface PaginationButtonProps extends ButtonBaseProps {
   active: boolean
 }
 
-export const PaginationButton = styled(ButtonBase, { shouldForwardProp: (prop) => prop !== 'active' })<Props>(
-  ({ active, theme }) => ({
-    color: theme.palette.grey[700],
-    padding: '5px 7px 5px 7px',
-    borderRadius: '5px',
-    fontFamily: 'inherit',
-    transition: theme.transitions.create(['background-color'], {
-      duration: '0.5s',
-    }),
-    ':hover': {
-      backgroundColor: theme.palette.divider,
-    },
-    ...(active && {
-      color: theme.palette.text,
-      backgroundColor: theme.palette.primary.light,
-      ':hover': {
-        backgroundColor: theme.palette.primary.light,
-      },
-    }),
+export const PaginationButton = styled(ButtonBase, {
+  shouldForwardProp: (prop) => prop !== 'active',
+})<PaginationButtonProps>(({ active, theme }) => ({
+  color: theme.palette.grey[700],
+  padding: '5px 7px 5px 7px',
+  borderRadius: '5px',
+  fontFamily: 'inherit',
+  transition: theme.transitions.create(['background-color'], {
+    duration: '0.5s',
   }),
-)
+  ':hover': {
+    backgroundColor: theme.palette.divider,
+  },
+  ...(active && {
+    color: theme.palette.text,
+    backgroundColor: theme.palette.primary.light,
+    ':hover': {
+      backgroundColor: theme.palette.primary.light,
+    },
+  }),
+}))
