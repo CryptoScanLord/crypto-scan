7235531ae910e3447ed02320334eff1a275800ac
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-08-25 21:16:24 +0300

    fix: container fix

--- client/shared/ui/container/src/index.tsx
+++ client/shared/ui/container/src/index.tsx
@@ -1,12 +1,10 @@
 import { Box } from '@mui/material'
 import { FC, PropsWithChildren } from 'react'
 
-const Container: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <Box maxWidth='1200px' margin='0 auto' display='flex' justifyContent='center' flexDirection='column'>
-      {children}
-    </Box>
-  )
-}
+const Container: FC<PropsWithChildren> = ({ children }) => (
+  <Box maxWidth='1200px' margin='0 auto' display='flex' justifyContent='center' flexDirection='column'>
+    {children}
+  </Box>
+)
 
 export default Container
