26dc419207db9669136f23e80fd471ce6100f1df
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-08-28 11:17:44 +0300

    fix: gap

--- client/shared/ui/container/src/index.tsx
+++ client/shared/ui/container/src/index.tsx
@@ -2,7 +2,7 @@ import Container from '@mui/material/Container'
 import { FC, PropsWithChildren } from 'react'
 
 const PageContainer: FC<PropsWithChildren> = ({ children }) => (
-  <Container component='div' sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+  <Container component='div' sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', gap: 1 }}>
     {children}
   </Container>
 )
