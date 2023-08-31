18fa6e785d0655970d6c6f9742c7cacd90b1f83e
BillyJean04 <bozhozda@gmail.com>
2023-08-31 09:33:11 +0300

    refactor: review fixes

--- crawler/bis/src/get-max-price.ts
+++ crawler/bis/src/get-max-price.ts
@@ -2,15 +2,14 @@ import * as Prv from './private-types.js'
 import * as Pub from './public-types.js'
 
 export async function getMaxPrice(collectionSlug: string): Promise<Pub.MaxPrice> {
-  const res: Prv.CollectionActivity[] = (await fetch(
+  const res: Prv.CollectionActivity[] = await fetch(
     `https://ordapi.bestinslot.xyz/v1/get_collection_activity/${collectionSlug}/8/1/0`,
-  ).then((res) => res.json())) satisfies Promise<Prv.CollectionActivity[]>
+  ).then((res) => res.json())
   const maxPrice: Prv.CollectionActivity = res.reduce(
     (maxPeak: Prv.CollectionActivity, peak: Prv.CollectionActivity) =>
       maxPeak.psbt_price > peak.psbt_price ? maxPeak : peak,
   )
-
   return {
     maxPrice: maxPrice.psbt_price,
-  } satisfies Pub.MaxPrice
+  }
 }
