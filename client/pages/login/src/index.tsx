51dee19e92c1b7ba732474e9d71b5fbd5fa0e689
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-09-01 18:02:37 +0300

    feat: add transactions

--- client/pages/login/src/index.tsx
+++ client/pages/login/src/index.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react'
 import { Box, Button } from '@mui/material'
 import { useLogin } from '@lib/auth-react'
+import { TransactionsPage } from '@page/transactions'
 
 export const LoginPage: FC = () => {
   const login = useLogin()
 
-  return (
-    <Box display='flex' justifyContent='center' flexDirection='column' height='100dvh' alignItems='center'>
-      <Button onClick={() => login()}>Login</Button>
-    </Box>
-  )
+  // return (
+  //   <Box display='flex' justifyContent='center' flexDirection='column' height='100dvh' alignItems='center'>
+  //     <Button onClick={() => login()}>Login</Button>
+  //   </Box>
+  // )
+  return <TransactionsPage />
 }
