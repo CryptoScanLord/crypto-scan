dddfd3388f82c497912a269368ec0659ac8a5b83
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-09-05 21:32:58 +0300

    feat: txs add

--- client/pages/transactions/src/index.tsx
+++ client/pages/transactions/src/index.tsx
@@ -15,6 +15,15 @@ export const TransactionsPage: FC = () => {
       getTransactions('bc1pcavtlcul2rcapxdr5dngafkcqcktv3wuj6rdqj40k952kqnf8qhqwrsax3', { limit: 0, offset: 0 }),
   })
 
+  console.log(transactions)
+
+  const dataTx = transactions?.map((el) => {
+    const method = el.delta > 0 ? 'Incoming' : 'Outgoing'
+    const from: string = el.inputs.length > 1 ? `${el.inputs.length} exits` : `${el.inputs[0]}`
+    const to: string = el.outputs.length > 1 ? `${el.outputs.length} exits` : `${el.outputs[0]}`
+    return { hash: el.id, method: method, from: from, to: to, value: el.delta }
+  })
+
   isTransactionsLoading && (
     <PageContainer sx={{ width: '100dvw', height: '100dvh', display: 'flex', alignItems: 'center' }}>
       <CircularProgress />
@@ -23,7 +32,12 @@ export const TransactionsPage: FC = () => {
 
   return (
     <PageContainer sx={{ height: '100dvh' }}>
-      <Table data={[]} headerCells={[]} subtitle='' title='Transactions' />
+      <Table
+        data={dataTx ? dataTx : []}
+        headerCells={['Transaction hash', 'Method', 'From', 'To', 'Value']}
+        subtitle=''
+        title='Transactions'
+      />
     </PageContainer>
   )
 }
