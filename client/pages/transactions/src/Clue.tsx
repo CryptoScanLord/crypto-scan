c8ee5bf884849630c5162aea532b7151bde1b2a9
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-09-06 22:20:00 +0300

    fix: key prop

--- client/pages/transactions/src/Clue.tsx
+++ client/pages/transactions/src/Clue.tsx
@@ -1,6 +1,6 @@
 import Tooltip from '@mui/material/Tooltip'
 import Box from '@mui/material/Box'
-import { Typography } from '@mui/material'
+import React from 'react'
 
 interface ClueProps {
   text: string[]
@@ -10,10 +10,10 @@ interface ClueProps {
 export const Clue = ({ text, exits }: ClueProps) => (
   <Tooltip
     title={text.map((el) => (
-      <>
+      <React.Fragment key={el}>
         {el}
         {`\n`}
-      </>
+      </React.Fragment>
     ))}
     arrow
   >
