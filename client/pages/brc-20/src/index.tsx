2cca95154420dd4d4dc9c1258014f0081d7b813d
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-09-15 20:26:52 +0300

    feat: add brc-20 page

--- client/pages/brc-20/src/index.tsx
+++ client/pages/brc-20/src/index.tsx
@@ -1,8 +1,21 @@
 import { FC } from 'react'
 import { useAuthGuard } from '@lib/auth-react'
+import { Table } from '@ui/table'
+import { useQuery } from '@tanstack/react-query'
+import { getTokens } from '@crawler/unisat'
 
 export const BRC20Page: FC = () => {
   useAuthGuard()
 
-  return <div />
+  const { data: tokens, isLoading: isTokensLoading } = useQuery({
+    queryKey: ['brc_20'],
+    queryFn: async () => {
+      const res = await getTokens('bc1pcavtlcul2rcapxdr5dngafkcqcktv3wuj6rdqj40k952kqnf8qhqwrsax3').then((data) => data)
+      return res
+    },
+  })
+
+  console.log(tokens)
+
+  return <Table data={[]} headerCells={['№', 'Name', 'Price']} title='BRC-20' subtitle='' />
 }
