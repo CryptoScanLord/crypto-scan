76a1ddd40f645c531c1cf7e9bf9b48592499087b
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-09-22 21:24:35 +0300

    fix/getTokens-fix

--- crawler/ordinals/src/getTokens.ts
+++ crawler/ordinals/src/getTokens.ts
@@ -1,7 +1,7 @@
 import { getTokenPrice } from './getTokenPrice'
 import { WalletRes } from './privateTypes'
 
-export async function getTokens(wallet: string) {
+export async function getTokens(wallet: string, volume: Record<string, string>) {
   const res: WalletRes = await fetch(`https://turbo.ordinalswallet.com/wallet/${wallet}`).then((data) => data.json())
   const filtered = res.brc20.filter((el) => el.available_balance > 0)
   const data = filtered.map(async (el, index) => ({
@@ -9,8 +9,8 @@ export async function getTokens(wallet: string) {
     name: el.ticker,
     amount: el.available_balance,
     floorPrice: el.floor_price_per ? el.floor_price_per : 0,
-    volume24: await getTokenPrice(el.ticker),
-    volumeAll: '',
+    volume24: volume.volume24,
+    volumeAll: volume.volumeAll,
   }))
   return data
 }
