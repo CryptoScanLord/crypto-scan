a141b720d58055afe2d28574203f286cf1f869ff
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-09-22 19:05:37 +0300

    fix/second-lint-fix

--- client/pages/brc-20/src/index.tsx
+++ client/pages/brc-20/src/index.tsx
@@ -1,6 +1,5 @@
-import { FC, useState, useEffect } from 'react'
+import { FC } from 'react'
 import { useAuthGuard } from '@lib/auth-react'
-import { getTokens } from '@crawler/ordinals'
 
 export const BRC20Page: FC = () => {
   useAuthGuard()
