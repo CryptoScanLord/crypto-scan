986c2c2aac11d93705d6f980f610c4b0066dc42f
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-09-22 19:03:41 +0300

    fix/lint-fix

--- client/pages/brc-20/src/index.tsx
+++ client/pages/brc-20/src/index.tsx
@@ -5,15 +5,5 @@ import { getTokens } from '@crawler/ordinals'
 export const BRC20Page: FC = () => {
   useAuthGuard()
 
-  const [s, sS] = useState([])
-
-  const f = async () => {
-    console.log(await getTokens('bc1pcavtlcul2rcapxdr5dngafkcqcktv3wuj6rdqj40k952kqnf8qhqwrsax3'))
-  }
-
-  useEffect(() => {
-    f()
-  }, [])
-
   return <div />
 }
