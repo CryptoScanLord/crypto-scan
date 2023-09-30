f9890e9b28e425db3d799d1581702b8f5fe83589
BillyJean04 <bozhozda@gmail.com>
2023-09-30 07:33:48 +0300

    feat: nfts page

--- client/pages/nfts/src/index.tsx
+++ client/pages/nfts/src/index.tsx
@@ -1,8 +1,27 @@
 import { FC } from 'react'
 import { useAuthGuard } from '@lib/auth-react'
+import { Table } from '@ui/table'
+import { useQuery } from '@tanstack/react-query'
+import CircularProgress from '@mui/material/CircularProgress'
 
 export const NFTsPage: FC = () => {
   useAuthGuard()
 
-  return <div />
+  const { data, isLoading } = useQuery({
+    queryKey: ['nfts'],
+    queryFn: () =>
+      fetch('http://localhost:8000/nfts/bc1pa0q6l7nyej0dh588swm49j3ddgevgyuxe75s2qal8lv0drnw8epqde2pe2').then((res) =>
+        res.json(),
+      ),
+  })
+
+  if (isLoading) return <CircularProgress />
+  return (
+    <Table
+      data={data}
+      headerCells={['Inscription', 'Collection Name', 'Image Url', 'Total Spent', 'Floor Price', 'Volume']}
+      subtitle=''
+      title='NFTs'
+    />
+  )
 }
