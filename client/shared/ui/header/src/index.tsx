013625a5f20b6732990e0e933cf8be0f44f6ac4d
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-10-12 15:38:03 +0300

    fix/lint-fix

--- client/shared/ui/header/src/index.tsx
+++ client/shared/ui/header/src/index.tsx
@@ -1,17 +1,23 @@
-import { FC } from 'react'
-import { useState } from 'react'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import { useNavigate } from 'react-router-dom'
-import { Search } from '@ui/search'
+import { FC } from "react";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+import { Search } from "@ui/search";
 
 export const Header: FC = () => {
-  const navigate = useNavigate()
-  const [wallet, setWallet] = useState<string>('')
+  const navigate = useNavigate();
+  const [wallet, setWallet] = useState<string>("");
   return (
-    <Box width='100%' height='100px' display='flex' alignItems='center' justifyContent='space-between'>
-      <Typography variant='h6'>OrdiTracker</Typography>
-      <Search value={wallet} handleChange={setWallet} navigate={navigate}  />
+    <Box
+      width="100%"
+      height="100px"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Typography variant="h6">OrdiTracker</Typography>
+      <Search value={wallet} handleChange={setWallet} navigate={navigate} />
     </Box>
-  )
-}
+  );
+};
--- client/shared/ui/search/src/index.tsx
+++ client/shared/ui/search/src/index.tsx
@@ -1,14 +1,13 @@
-import { ChangeEvent } from 'react'
-import { FC }          from 'react'
+import { ChangeEvent } from "react";
+import { FC } from "react";
 
-import FormControl     from '@mui/material/FormControl'
-import Input           from '@mui/material/Input'
-import InputAdornment  from '@mui/material/InputAdornment'
+import FormControl from "@mui/material/FormControl";
+import Input from "@mui/material/Input";
+import InputAdornment from "@mui/material/InputAdornment";
 
 export interface SearchProps {
-  value: string
-  handleChange: (e: string) => void
-  navigate?: any
+  value: string;
+  handleChange: (e: string) => void;
 }
 
 export const Search: FC<SearchProps> = ({ value, handleChange }) => (
@@ -16,22 +15,30 @@ export const Search: FC<SearchProps> = ({ value, handleChange }) => (
     <Input
       value={value}
       // onChange={(e: ChangeEvent<HTMLInputElement>) => e.keyCode === 13 ? navigate(`/portfolio/${value}`) : handleChange(e.target.value)}
-      onChange={(e: ChangeEvent<HTMLInputElement>) => handleChange(e.target.value)}
-      placeholder='Search'
+      onChange={(e: ChangeEvent<HTMLInputElement>) =>
+        handleChange(e.target.value)
+      }
+      placeholder="Search"
       startAdornment={
-        <InputAdornment position='start'>
-          <svg xmlns='http://www.w3.org/2000/svg' width='15' height='15' viewBox='0 0 24 24' fill='none'>
+        <InputAdornment position="start">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="15"
+            height="15"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
             <path
-              d='M11.5 21.75C5.85 21.75 1.25 17.15 1.25 11.5C1.25 5.85 5.85 1.25 11.5 1.25C17.15 1.25 21.75 5.85 21.75 11.5C21.75 17.15 17.15 21.75 11.5 21.75ZM11.5 2.75C6.67 2.75 2.75 6.68 2.75 11.5C2.75 16.32 6.67 20.25 11.5 20.25C16.33 20.25 20.25 16.32 20.25 11.5C20.25 6.68 16.33 2.75 11.5 2.75Z'
-              fill='#525F7A'
+              d="M11.5 21.75C5.85 21.75 1.25 17.15 1.25 11.5C1.25 5.85 5.85 1.25 11.5 1.25C17.15 1.25 21.75 5.85 21.75 11.5C21.75 17.15 17.15 21.75 11.5 21.75ZM11.5 2.75C6.67 2.75 2.75 6.68 2.75 11.5C2.75 16.32 6.67 20.25 11.5 20.25C16.33 20.25 20.25 16.32 20.25 11.5C20.25 6.68 16.33 2.75 11.5 2.75Z"
+              fill="#525F7A"
             />
             <path
-              d='M22.0004 22.7499C21.8104 22.7499 21.6204 22.6799 21.4704 22.5299L19.4704 20.5299C19.1804 20.2399 19.1804 19.7599 19.4704 19.4699C19.7604 19.1799 20.2404 19.1799 20.5304 19.4699L22.5304 21.4699C22.8204 21.7599 22.8204 22.2399 22.5304 22.5299C22.3804 22.6799 22.1904 22.7499 22.0004 22.7499Z'
-              fill='#525F7A'
+              d="M22.0004 22.7499C21.8104 22.7499 21.6204 22.6799 21.4704 22.5299L19.4704 20.5299C19.1804 20.2399 19.1804 19.7599 19.4704 19.4699C19.7604 19.1799 20.2404 19.1799 20.5304 19.4699L22.5304 21.4699C22.8204 21.7599 22.8204 22.2399 22.5304 22.5299C22.3804 22.6799 22.1904 22.7499 22.0004 22.7499Z"
+              fill="#525F7A"
             />
           </svg>
         </InputAdornment>
       }
     />
   </FormControl>
-)
+);
