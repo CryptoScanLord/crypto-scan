e4b079b5ef95fc1c7f68fb1f1cc338da2151dd34
maslakovSaveliy <saveliymaslakov969@gmail.com>
2023-10-20 17:07:39 +0300

    fix/search-fix

--- client/app/root/src/index.tsx
+++ client/app/root/src/index.tsx
@@ -1,15 +1,20 @@
-import type { FC }            from 'react'
+import type { FC } from "react";
 
-import      { withProviders } from '@app/providers'
-import      { Routes }        from '@app/routes'
-import      { Container }     from '@ui/container'
-import      { Header }        from '@ui/header'
+import { withProviders } from "@app/providers";
+import { Routes } from "@app/routes";
+import { Container } from "@ui/container";
+import { Header } from "@ui/header";
+import { useParams } from "react-router-dom";
 
-const App: FC = () => (
-	<Container>
-		<Header />
-		<Routes />
-	</Container>
-)
+const App: FC = () => {
+	const { wallet } = useParams();
+	console.log(wallet);
+	return (
+		<Container>
+			<Header />
+			<Routes />
+		</Container>
+	);
+};
 
-export default withProviders(App)
+export default withProviders(App);
